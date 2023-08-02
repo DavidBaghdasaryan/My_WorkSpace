c1a8734cac98310bc6881b73d23c651ecff74721
david.baghdasaryan87@gmail.com <david.baghdasaryan87@gmail.com>
2023-08-02 16:20:57 +0400

    ch

--- My_WorkSpace/Models/Employees.cs
+++ My_WorkSpace/Models/Employees.cs
@@ -8,7 +8,7 @@ namespace My_WorkSpace.Models
 {
    public class Employees
     {
-        public int Id { get; set; }
+        public int Id { get; set; } 
         public string Name { get; set; }
         public string LastName { get; set; }
     }
